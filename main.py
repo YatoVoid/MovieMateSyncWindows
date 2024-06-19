253312e5e6db4a18015e1d0b2f3016bb755634c4
YatoVoid <98448225+YatoVoid@users.noreply.github.com>
2024-06-19 13:57:42 +0400

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,21 @@ import tkinter as tk
 import settings as st
 import host
 import viewer
-import threading
 import sys
+import os
+import threading
+os.add_dll_directory(r'C:\Program Files\VideoLAN\VLC')
 
-def open_movieM_Host(menu):
+def thread_create(function):
+    threading.Thread(target=function).start()
+    threading.Thread(target=function).join()
+
+def open_movieM_Host1(menu):
     menu.destroy()
     host.open_movieM_Host()
 
-def open_movieM_Viewer(menu):
+
+def open_movieM_Viewer1(menu):
     menu.destroy()
     viewer.open_movieM_Viewer()
 
@@ -24,16 +31,18 @@ def open_movieM_Menu():
 
     menu = ctk.CTk()
     menu.geometry("680x430")
+    menu.title("MovieMateSyncMenu")
+
     menu.minsize(680, 430)
 
     # Handle window close event
     menu.protocol("WM_DELETE_WINDOW", on_closing)
 
-    button_H = ctk.CTkButton(master=menu, text="Host Room", corner_radius=30, command=lambda: open_movieM_Host(menu),
+    button_H = ctk.CTkButton(master=menu, text="Host Room", corner_radius=30, command=lambda: open_movieM_Host1(menu),
                              height=240, width=240, font=(st.default_font_family, st.font_sizes["large"]))
     button_H.place(relx=0.25, rely=0.5, anchor=ctk.CENTER)
 
-    button_J = ctk.CTkButton(master=menu, text="Join Room", corner_radius=30, command=lambda: open_movieM_Viewer(menu),
+    button_J = ctk.CTkButton(master=menu, text="Join Room", corner_radius=30, command=lambda: open_movieM_Viewer1(menu),
                              height=240, width=240, font=(st.default_font_family, st.font_sizes["large"]))
     button_J.place(relx=0.75, rely=0.5, anchor=ctk.CENTER)
 
